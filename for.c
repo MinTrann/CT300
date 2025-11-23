5d1b82f2e81ec3c49d95bfc19f5c12d12ed0f376
Min Tran <nhatmin1230@gmail.com>
2025-11-23 15:19:12 +0700

    Add for.c and for.exe

---
+++ for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    printf("Cac so tu 1 den 10: ");
+    
+    for(i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    
+    printf("\n");
+    return 0;
+}
